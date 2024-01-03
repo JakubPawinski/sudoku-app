2ff4d14a462d775bc55dc47c4494b2640ce7099b
Jakub Pawiński <j.pawinski.164@studms.ug.edu.pl>
2024-01-03 10:12:43 +0100

    Add drawing sudoku grid

--- main.py
+++ main.py
@@ -8,12 +8,19 @@ import json
 pygame.init()
 
 # Main variables
-screen_size = (750, 750)
+screen_size = (500, 600)
+run = True
 
 # Colours
 white = (255, 255, 255)
 black = (0, 0, 0)
+background_colour = (250, 250, 250)
+
+#fonts
+font_grid = pygame.font.SysFont(None, 40)
+
 #global variables
+dif = 500/9
 
 # Screen init
 screen = pygame.display.set_mode((screen_size))
@@ -22,6 +29,7 @@ pygame.display.set_caption("Sudoku")
 
 def get_sudoku_grid(difficulty):
     # The function gets the sudoku board from api and returns both the sudoku board and its solution
+
     api_url = 'https://sudoku-api.vercel.app/api/dosuku'
     queries = {
         'newboard': 'newboard',
@@ -31,18 +39,56 @@ def get_sudoku_grid(difficulty):
         'message': 'message'                 
       }
     
-
     response = requests.get(api_url, params=queries)
     data = response.json()
     # Create dictionary with solution and the value of the sudoku board
     data = {'value': data['newboard']['grids'][0]['value'], 'solution': data['newboard']['grids'][0]['solution']}
     return data
 
+#example grid
+test_grid =[
+        [7, 8, 0, 4, 0, 0, 1, 2, 0],
+        [6, 0, 0, 0, 7, 5, 0, 0, 9],
+        [0, 0, 0, 6, 0, 1, 0, 7, 8],
+        [0, 0, 7, 0, 4, 0, 2, 6, 0],
+        [0, 0, 1, 0, 5, 0, 9, 3, 0],
+        [9, 0, 4, 0, 6, 0, 0, 0, 5],
+        [0, 7, 0, 3, 0, 0, 0, 1, 2],
+        [1, 2, 0, 0, 0, 7, 4, 0, 0],
+        [0, 4, 9, 2, 0, 6, 0, 0, 7]
+    ]
 
+def draw_grid(board):
+    screen.fill(background_colour)
+    for i in range(9):
+        for j in range(9):
+            if board[i][j] != 0:
+                text1 = font_grid.render(str(board[i][j]), 1, (0, 0, 0))
+                screen.blit(text1, (i * dif + 15, j * dif + 15))
+
+    for i in range(10):
+        if i % 3 == 0 :
+            thick = 7
+        else:
+            thick = 1
+        pygame.draw.line(screen, black, (0, i * dif), (500, i * dif), thick)
+        pygame.draw.line(screen, black, (i * dif, 0), (i * dif, 500), thick)  
+
+
+def main_menu():
+    print("Main menu")
 
 def game():
-    pprint(get_sudoku_grid("Medium"))
+    board = get_sudoku_grid("Medium")
+    pprint(board)
+    while run:
+        draw_grid(test_grid)
+        pygame.display.update()
 
+def main():
+    # The main project funtion
+
+    game()
 
 if __name__ == "__main__":
-    game()
+    main()
