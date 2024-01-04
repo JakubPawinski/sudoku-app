18d05366517dbba6801f7eb2bf3d9fd1f22bd8e6
Jakub Pawiński <j.pawinski.164@studms.ug.edu.pl>
2024-01-04 13:28:15 +0100

    Add highlighting cells

--- main.py
+++ main.py
@@ -17,15 +17,76 @@ white = (255, 255, 255)
 black = (0, 0, 0)
 background_colour = (250, 250, 250)
 
+highlited_colour = (177, 219, 238)
+highlited_colour_background = (230, 230, 230)
+
 #fonts
 font_grid = pygame.font.SysFont(None, 40)
 
 #global variables
 grid_gap = screen_size[0]/9
+cords = [0, 0]
+
 
 # Screen init
 screen = pygame.display.set_mode((screen_size))
 pygame.display.set_caption("Sudoku")
+
+#test funtion
+def debug_mouse_position():
+    debug_pos = pygame.mouse.get_pos()
+    print(debug_pos)
+
+def get_cords(pos):
+    #This function gets cords of highlighted cell
+    temp = [0, 0]
+    temp[0] = pos[0] // grid_gap
+    temp[1] = pos[1] // grid_gap
+    print(temp)
+    return temp
+
+def get_highlighted_box_cords(cords):
+    #This function gets the box cords from highlited cell
+    if cords[0] >= 0 and cords[0] <= 2:
+        if cords[1] >= 0 and cords[1] <= 2:
+            return [0, 0]
+        elif cords[1] >= 3 and cords[1] <= 5:
+            return [0, 3]
+        elif cords[1] >= 6 and cords[1] <= 8:
+            return [0, 6]
+    elif cords[0] >= 3 and cords[0] <= 5:
+        if cords[1] >= 0 and cords[1] <= 2:
+            return [3, 0]
+        elif cords[1] >= 3 and cords[1] <= 5:
+            return [3, 3]
+        elif cords[1] >= 6 and cords[1] <= 8:
+            return[3, 6]
+    elif cords[0] >= 6 and cords[0] <= 8:
+        if cords[1] >= 0 and cords[1] <= 2:
+            return [6, 0]
+        elif cords[1] >= 3 and cords[1] <= 5:
+            return[6, 3]
+        elif cords[1] >= 6 and cords[1] <= 8:
+            return[6, 6]
+    else:
+        return cords
+
+def draw_highlighted_cells():
+    #This function draw highlited cells
+
+    print(cords)
+    #Check if it is possible to highlight the cells
+    if cords[0] <= 8 and cords[1] <= 8:
+        box_cords = get_highlighted_box_cords(cords)
+        #highlited box
+        pygame.draw.rect(screen, highlited_colour_background, (box_cords[0] * grid_gap, box_cords[1] * grid_gap, grid_gap * 3, grid_gap * 3))
+        #vertical line
+        pygame.draw.rect(screen, highlited_colour_background, (cords[0] * grid_gap, 0, grid_gap, 9 * grid_gap))
+        #horizontal line
+        pygame.draw.rect(screen, highlited_colour_background, (0, cords[1] * grid_gap, grid_gap * 9, grid_gap))
+        #Highlited cell
+        pygame.draw.rect(screen, highlited_colour, (cords[0] * grid_gap, cords[1] * grid_gap, grid_gap, grid_gap))
+
 
 
 def get_sudoku_grid(difficulty):
@@ -62,7 +123,6 @@ test_grid =[
 def draw_grid(board):
     #This function draws the sudoku board
 
-    screen.fill(background_colour)
     for i in range(9):
         for j in range(9):
             if board[i][j] != 0:
@@ -77,26 +137,34 @@ def draw_grid(board):
         pygame.draw.line(screen, black, (0, i * grid_gap), (500, i * grid_gap), thick)
         pygame.draw.line(screen, black, (i * grid_gap, 0), (i * grid_gap, 500), thick)  
 
-
-
-
-
 def main_menu():
     print("Main menu")
 
 def game():
+    global cords
+
     run = True
 
     board = get_sudoku_grid("Medium")
     pprint(board)
 
     while run:
+        #This section draws the highlighted cells in the correct order in order to draw highlight behind the sudoku grid
+        screen.fill(background_colour)
+        draw_highlighted_cells()
         draw_grid(test_grid)
+
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
-        
-        
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                pos = pygame.mouse.get_pos()
+                cords = get_cords(pos)
+
+
+        #test funtion
+        # debug_mouse_position()
         
         pygame.display.update()
 
